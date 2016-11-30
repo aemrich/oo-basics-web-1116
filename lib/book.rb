7751af7f80e861eb00bc38c1b4799774833ccf84
aemrich <adam.emrich@gmail.com>
2016-11-30 14:03:26 -0500

    done

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,17 @@
+class Book
+  def initialize(title, author="nil", page_count=0, genre=nil)
+    @title = title
+    @author = author
+    @page_count = page_count
+    @genre = genre
+  end
 
+  attr_accessor :author
+  attr_accessor :title
+  attr_accessor :page_count
+  attr_accessor :genre
 
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+end
